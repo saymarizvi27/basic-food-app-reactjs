c520ad9472ea9b57e669e680ef85789c420ace45
saymarizvi <rizvisayma@yahoo.com>
2021-06-03 01:53:17 +0530

    adding header to the app

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.Module.css";
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      {console.log(classes,"ayeee")}  
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious food" />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
